747080d1d06de1c1462c18818489d5cf1cb242db
Chronicle Codes <chronicle.codes@gmail.com>
2020-08-06 11:51:57 -0500

    Created Window Class

---
+++ src/com/minesweeper/GameWindow.java
@@ -0,0 +1,11 @@
+package com.minesweeper;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame{
+	private static final long serialVersionUID = 6009903486855646095L;
+
+	public GameWindow() {
+		
+	}
+}
