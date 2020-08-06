a14b38c045069060faa5cd253b7a8f687628acde
Chronicle Codes <chronicle.codes@gmail.com>
2020-08-06 13:27:40 -0500

    Creates Game Window

--- src/com/minesweeper/GameWindow.java
+++ src/com/minesweeper/GameWindow.java
@@ -1,11 +1,41 @@
 package com.minesweeper;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameWindow extends JFrame{
 	private static final long serialVersionUID = 6009903486855646095L;
-
-	public GameWindow() {
+	private int rows;
+	private int cols;
+	private int cellSize;
+	
+	public GameWindow(int w, int h, int cellSize) {
+		rows = h;
+		cols = w;
+		this.cellSize = cellSize;
+		
+		setTitle("Minesweeper");
+		
+		//width x height
+		Dimension canvas = new Dimension(cellSize * cols, cellSize * rows);
+		JPanel content = (JPanel) getContentPane();
+		
+		content.setPreferredSize(canvas);
+		
+		addComponents();
+		
+		pack();
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		validate();
+	}
+	
+	private void addComponents() {
 		
 	}
 }
--- src/com/minesweeper/Main.java
+++ src/com/minesweeper/Main.java
@@ -3,6 +3,7 @@ package com.minesweeper;
 public class Main {
 	public static void main(String[] args) {
 		//Create game window
+		GameWindow window = new GameWindow(50, 25, 20);
 		
 		//Initialize game loop
 		
